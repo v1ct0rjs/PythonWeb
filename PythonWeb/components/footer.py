3b2da255da6dadcad649b735e4822cd3608fbe01
v1ct0r <vmjimenezs02@educarex.es>
2024-05-26 11:13:00 +0200

    Links on footer.py

--- PythonWeb/components/footer.py
+++ PythonWeb/components/footer.py
@@ -7,13 +7,26 @@ def footer() -> rx.Component:
     return rx.vstack(
         rx.center(
             rx.hstack(
-                rx.image(
-                    src="python.svg",
-                    width="100px",
-                    padding_right=Size.XLARGE.value,
-                    alt="Logo de Python, dos serpientes entrelazadas"),  # Aumenta este valor para más espacio
+                rx.link(
+                    rx.image(
+                        src="python.svg",
+                        width="100px",
+                        padding_right=Size.XLARGE.value,
+                        alt="Logo de Python, dos serpientes entrelazadas"
+                    ),
+                    href="https://www.python.org/",
+                    is_external=True
+                ),
+                rx.link(
                 rx.image(src="Tux_Mono.png", width="60px", alt="Logo de Tux, la mascota de Linux un pinguino"),
+                href="https://www.kernel.org/",
+                is_external=True
+                ),
+                rx.link(
                 rx.image(src="Reflex_white.svg", width="150px", padding_left=Size.XLARGE.value, alt="Logo de reflex"),
+                href="https://reflex.dev/",
+                is_external=True
+                ),
                 align="center"
             )
         ),
