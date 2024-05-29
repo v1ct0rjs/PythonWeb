16470b7bf1d7f55c7c71f56459502bacfdcc10b5
v1ct0r <vmjimenezs02@educarex.es>
2024-05-29 19:40:18 +0200

    new scripts

--- local_build.sh
+++ local_build.sh
@@ -3,9 +3,9 @@
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-reflex init
-API_URL=https://api.v1ct0r.dev reflex export --frontend-only
 rm -rf public
+reflex init
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
---
+++ remote_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd PythonWeb
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL=https://api.v1ct0r.dev reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
