9939b206a2b1d2d90afa56df02169b446a8bea20
v1ct0rjs <vmjimenezs02@educarex.es>
2024-05-31 10:11:05 +0200

    rxconfig.py test

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="PythonWeb",
-    cors_allowed_origins=[
-        "https://api.v1ct0r.dev",
-        "http://localhost:3000",
-        "https://www.v1ct0r.dev"
-    ]
+#    cors_allowed_origins=[
+#        "https://api.v1ct0r.dev",
+#        "http://localhost:3000",
+#        "https://www.v1ct0r.dev"
+#    ]
 )
