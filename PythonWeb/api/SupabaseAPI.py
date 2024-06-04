c7bec69a5cf7c9dfe6c130d5f61d62a9c65be657
v1ct0r <vmjimenezs02@educarex.es>
2024-06-04 21:38:34 +0200

    Vercel Fix deploit

--- PythonWeb/api/SupabaseAPI.py
+++ PythonWeb/api/SupabaseAPI.py
@@ -2,14 +2,24 @@ import os
 import dotenv
 from supabase import create_client, Client
 
-class SupabaseAPI:
 
+class SupabaseAPI:
     dotenv.load_dotenv()
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-    def  featured(self) -> list:
+    def __init__(self):
+        self.supabase: Client = None
+
+    def create_client(self):
+        if self.supabase is None:
+            self.supabase = create_client(self.SUPABASE_URL, self.SUPABASE_KEY)
+
+    def featured(self) -> list:
+
+        if self.supabase is None:
+            self.create_client()
+
         response = self.supabase.table('featured').select('*').execute()
 
         featured_data = []
@@ -17,5 +27,5 @@ class SupabaseAPI:
         if len(response.data) > 0:
             for featured_items in response.data:
                 featured_data.append(featured_items)
-        #print(featured_data)
+        # print(featured_data)
         return featured_data
