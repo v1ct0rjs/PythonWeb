862a127f5a10930fdb7c359b9812f8d39017432b
v1ct0r <vmjimenezs02@educarex.es>
2024-06-04 17:17:56 +0200

    2º Test

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="PythonWeb",
-    api_url="https://api.v1ct0r.dev:8000",
+    api_url="https://api.v1ct0r.dev",
     cors_allowed_origins=[
         "https://api.v1ct0r.dev",
         "http://localhost:3000",
