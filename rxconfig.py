98f0e610ba0d71a89842cb244beb177d49a85874
v1ct0r <vmjimenezs02@educarex.es>
2024-06-04 17:40:53 +0200

    4 test Railway

--- rxconfig.py
+++ rxconfig.py
@@ -4,8 +4,9 @@ config = rx.Config(
     app_name="PythonWeb",
     api_url="https://api.v1ct0r.dev",
     cors_allowed_origins=[
+        "https://www.v1ct0r.dev",
         "https://api.v1ct0r.dev",
-        "http://localhost:3000",
+        "http://localhost:3000"
     ]
 )
 
