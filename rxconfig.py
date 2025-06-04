1a28cfc4733648b95e090e281d0db4e25c46b834
v1ct0rjs <vmjimenezs02@educarex.es>
2025-06-04 11:53:03 +0200

    rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -1,24 +1,22 @@
 import reflex as rx
 import os
 
-# Temporalmente, para asegurar que en producción la API_URL sea nula
-# Asumimos que si se está construyendo con Vercel, es 'prod'
-# O puedes basarte en una variable que Vercel sí establece por defecto, como VERCEL_ENV
-
-prod_environment = os.getenv("VERCEL_ENV") == "production" or os.getenv("REFLEX_ENV") == "prod"
-
-final_api_url = "" if prod_environment else "http://localhost:8000"
-
+# Configuración para diferentes entornos
+def get_api_url():
+  # Como no hay backend, retornamos una cadena vacía o None.
+  # Una cadena vacía suele ser segura para `reflex export`.
+  return ""
 
 config = rx.Config(
-    app_name="PythonWeb",
-    api_url=final_api_url, # Usar la URL determinada
-    cors_allowed_origins=[
-        "https://www.v1ct0r.dev",
-        "https://victorjs-web.vercel.app",
-        "http://localhost:3000"
-    ],
-    frontend_port=3000
+  app_name="PythonWeb",
+  api_url="", # Esto ahora será ""
+  cors_allowed_origins=[
+      "https://www.v1ct0r.dev",
+      "https://victorjs-web.vercel.app",
+      "http://localhost:3000"
+  ],
+  #backend_port=int(os.getenv("PORT", "8000")),
+  frontend_port=3000
 )
 
 
