606c414076a17ba934450318d982f06b95fc35aa
Víctor Jiménez <149415251+v1ct0rjs@users.noreply.github.com>
2025-06-30 13:07:24 +0200

    Update PythonWeb.py

--- PythonWeb/PythonWeb.py
+++ PythonWeb/PythonWeb.py
@@ -2,11 +2,11 @@ import reflex as rx
 from PythonWeb.styles.styles import *
 from PythonWeb.constants import *
 
-# Importar los módulos de las páginas para que Reflex las descubra
+
 import PythonWeb.pages.index
 import PythonWeb.pages.english
 
-from PythonWeb.api.api import repo, live # Asumiendo que estas son rutas API
+from PythonWeb.api.api import repo, live 
 
 app = rx.App(
     style=BASE_STYLE,
@@ -23,12 +23,5 @@ app = rx.App(
     ],
 )
 
-# No necesitas app.add_page(index) o app.add_page(eng) explícitamente
-# si los decoradores @rx.page se usan sin argumentos de app,
-# ya que se registran con la instancia de app predeterminada.
-# Si has pasado app=app al decorador, entonces esta forma es correcta.
-# Si no, Reflex las encontrará porque los módulos han sido importados.
 
-# app.api.add_api_route("/repo", repo)
-# app.api.add_api_route("/live/{user}", live)
 
